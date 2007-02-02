9191cff24820e05ed06b10c7ba5aae380f0029fe
Chris Williams <chris@bingosnet.co.uk>
2007-02-02 13:20:15 +0000

    Import of BINGOS/Bundle-POE-IRC-1.00 from CPAN.
    gitpan-cpan-distribution: Bundle-POE-IRC
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         BINGOS/Bundle-POE-IRC-1.00.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/POE/IRC.pm
@@ -0,0 +1,38 @@
+package Bundle::POE::IRC;
+
+use strict;
+use vars qw($VERSION);
+
+$VERSION = '1.00';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::POE::IRC - Bundle to install all POE related IRC modules.
+
+=head1 SYNOPSIS
+
+    perl -MCPANPLUS -e 'install Bundle::POE::IRC'
+
+=head1 DESCRIPTION
+
+Bundle::POE::IRC - Bundle to install all POE related IRC modules and optional dependencies.
+
+=head1 CONTENTS
+
+POE 0.38
+
+POE::Component::Client::DNS 0.99
+
+POE::Component::IRC
+
+POE::Component::Server::IRC
+
+=head1 AUTHOR
+
+Chris Williams <chris@bingosnet.co.uk>
+
+=cut
